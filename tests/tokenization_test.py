af6a3c5969facebb423d4c4d0a655efca3fdb5c0
Kyle Silver <kyleAsilver@gmail.com>
2022-06-05 19:48:33 -0400

    a(n) passing unit test

--- tests/tokenization_test.py
+++ tests/tokenization_test.py
@@ -9,7 +9,9 @@ from jsonmap.parse.tokens import BareWord, ReferenceToken, Symbol, SymbolToken
 class SingleStatementTokenization(TestCase):
     def test_simple_assignment(self) -> None:
         actual = tokens.tokenize("lhs = &rhs;")
-        assert actual[0] == BareWord(0, "lhs")
+        self.assertEquals(actual[0], BareWord(0, "lhs"))
         assert actual[1] == SymbolToken(4, Symbol.assignment)
         assert actual[2] == ReferenceToken(7, path=["rhs"])
         assert actual[3] == SymbolToken(10, Symbol.end_of_statement)
+    
+    def test_colon_assignment(self)
