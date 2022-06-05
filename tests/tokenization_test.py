7327464164932aa029fd686a6d34e1d8830afdbc
Kyle Silver <kyleAsilver@gmail.com>
2022-06-05 19:49:59 -0400

    unittest assertions

--- tests/tokenization_test.py
+++ tests/tokenization_test.py
@@ -9,9 +9,7 @@ from jsonmap.parse.tokens import BareWord, ReferenceToken, Symbol, SymbolToken
 class SingleStatementTokenization(TestCase):
     def test_simple_assignment(self) -> None:
         actual = tokens.tokenize("lhs = &rhs;")
-        self.assertEquals(actual[0], BareWord(0, "lhs"))
-        assert actual[1] == SymbolToken(4, Symbol.assignment)
-        assert actual[2] == ReferenceToken(7, path=["rhs"])
-        assert actual[3] == SymbolToken(10, Symbol.end_of_statement)
-    
-    def test_colon_assignment(self)
+        self.assertEqual(actual[0], BareWord(0, "lhs"))
+        self.assertEqual(actual[1], SymbolToken(4, Symbol.assignment))
+        self.assertEqual(actual[2], ReferenceToken(7, path=["rhs"]))
+        self.assertEqual(actual[3], SymbolToken(10, Symbol.end_of_statement))
